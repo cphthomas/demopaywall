0b43674250595d3029ddbdf97e552c82cda3b78a
007AMAN007 <as47986@gmail.com>
2021-02-18 22:10:42 +0530

    change plans

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,40 +1,16 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { faunaFetch } = require('./utils/fauna');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const { faunaFetch } = require("./utils/fauna");
 
 exports.handler = async (event) => {
-  const { user } = JSON.parse(event.body);
-
-  // create a new customer in Stripe
-  const customer = await stripe.customers.create({ email: user.email });
-
-  // subscribe the new customer to the free plan
-  await stripe.subscriptions.create({
-    customer: customer.id,
-    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
-  });
-
-  // store the Netlify and Stripe IDs in Fauna
-  await faunaFetch({
-    query: `
-      mutation ($netlifyID: ID!, $stripeID: ID!) {
-        createUser(data: { netlifyID: $netlifyID, stripeID: $stripeID }) {
-          netlifyID
-          stripeID
-        }
-      }
-    `,
-    variables: {
-      netlifyID: user.id,
-      stripeID: customer.id,
+  fetch("/.netlify/functions/create-manage-link", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${user.token.access_token}`,
     },
-  });
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      app_metadata: {
-        roles: ['free'],
-      },
-    }),
-  };
+  })
+    .then((res) => res.json())
+    .then((link) => {
+      window.location.href = link;
+    })
+    .catch((err) => console.error(err));
 };
