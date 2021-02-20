e72fb7d5e4c81e423e92f68c5e655c4832e6348c
007AMAN007 <as47986@gmail.com>
2021-02-20 18:27:20 +0530

    fix css

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,36 @@
 console.log("in dataset page custom.js up");
 const handleUserStateChange = async (user) => {
   if (user) {
-    $("body").show();
+    const token = user ? await netlifyIdentity.currentUser().jwt(true) : false;
+    const type = "";
+    fetch("/.netlify/functions/get-user-role", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ type }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data[0]);
+        if (data[0] == "free") {
+          fetch("/.netlify/functions/create-manage-link", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${user.token.access_token}`,
+            },
+          })
+            .then((res) => res.json())
+            .then((link) => {
+              window.location.href = link;
+            })
+            .catch((err) => console.error(err));
+        } else {
+          if (data[0] == "pro") {
+            $("body").show();
+          }
+        }
+      });
   } else {
     document.location.href = "/";
   }
